6a95e55f580906941003a6b986760c79935450f1
Juanfrancisc <juanfrancisc@hotmail.com>
2022-09-16 10:46:05 +0200

    Añadidas las lineas de ReacTostify en el fichero App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import Spinner from "../src/components/Spinner/Spinner";
 
 //Importamos BrowserRouter, Routes, Route para el trabajo de rutas
@@ -14,6 +16,10 @@ function App() {
       <main>
           <Spinner />
       </main>
+
+      {/* Para mostrar las alertas */}
+      <ToastContainer position="bottom-center" theme="dark" />
+
       <footer>
 
       </footer>
