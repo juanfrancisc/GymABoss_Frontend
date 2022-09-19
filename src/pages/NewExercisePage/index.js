cb8a2220f9cd523734dc23dea31d7276fd726dc1
davizdelarosa <108464629+davizdelarosa@users.noreply.github.com>
2022-09-19 09:27:54 +0200

    NewExercisePage subido sin css

---
+++ src/pages/NewExercisePage/index.js
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom";
+import NewExerciseForm from "../../components/NewExerciseForm";
+import { useTokenContext } from "../../contexts/TokenContext";
+
+const NewExerciseForm = () => {
+  const { token } = useTokenContext();
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <section>
+      <h2>New Exercise</h2>
+
+      <NewExerciseForm />
+    </section>
+  );
+};
+
+export default NewExercisePage;
