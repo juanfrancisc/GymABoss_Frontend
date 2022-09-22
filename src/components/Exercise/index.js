bd3e8367d98a9a2e057772732b3b67c1d12ad4e0
davizdelarosa <108464629+davizdelarosa@users.noreply.github.com>
2022-09-22 12:44:26 +0200

    Exercise con ss

--- src/components/Exercise/index.js
+++ src/components/Exercise/index.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 //import { useTokenContext } from "../../contexts/TokenContext";
 //import { toast } from "react-toastify";
-
+import "./styles.css";
+import flexiones from "../../assets/imagenes/flexiones.jpg";
 const Exercise = ({ exercise }) => {
   //console.log(exercise)
   const { id, idUser, n_like, title, description, photo, typology } = exercise;
@@ -10,18 +11,19 @@ const Exercise = ({ exercise }) => {
   //console.log(photo)
 
   return (
-    <section className="exercise">
-      <h3>Titulo: {title}</h3>
-      <p>Tipologia: {typology}</p>
-      <p>description: {description}</p>
-      <p>Numero de Like's: {n_like}</p>
-      {photo.length > 0 && (
+    <section className="boxes">
+      <h3 className="title">{title}</h3>
+      <p className="typology">{typology}</p>
+      <img height="180" src={flexiones}></img>
+      {/* {photo.length > 0 && (
         <img
-          //src={`${process.env.REACT_APP_API_URL}/exercises/${photo[0].title}`}
+          
           src={`${process.env.REACT_APP_API_URL}/imagenes/${photo}`}
           alt={title}
         />
-      )}
+      )} */}
+
+      <p className="nLikes">Likes: {n_like}</p>
     </section>
   );
 };
