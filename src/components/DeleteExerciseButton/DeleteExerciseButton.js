91ab6752fa130d39e3ef126a8868dbb4cbb99584
Juanfrancisc <juanfrancisc@hotmail.com>
2022-09-24 10:57:10 +0200

    Funcion DeleteEjercios comprobada e integrada

--- src/components/DeleteExerciseButton/DeleteExerciseButton.js
+++ src/components/DeleteExerciseButton/DeleteExerciseButton.js
@@ -1,10 +1,12 @@
 import { toast } from "react-toastify";
 import Trash from "../../assets/imagenes/trash.png"
 import { useTokenContext } from "../../contexts/TokenContext";
+/* import { useState } from "react"; */
+import { useNavigate } from "react-router-dom";
+import './DeleteExerciseButton.css'
 
-const deleteExerciseID = async (id) => {
-  const {token} = useTokenContext
-  console.log(token)
+const deleteExerciseID = async (id,token) => {
+  
   try {
   const res = await fetch(`${process.env.REACT_APP_API_URL}/deleteExercise/${id.id}`, {
     method: "DELETE",
@@ -12,34 +14,34 @@ const deleteExerciseID = async (id) => {
       authorization: token,
     }
   })
-  console.log(res);
+  //console.log(res);
   const body = await res.json();
 
   if (!res.ok) {
-    throw new Error(body.message);
+            throw new Error(body.message);
   }
-
-  console.log(body)
+   //console.log(body)
 
   toast.success(body.message)
-
 } catch (error) {
   console.error(error.message);
   toast.error(error.message);
 }
 
-}
+};
 
 const DeteleExerciseButton = (id) => {
-  
   //useDeteleExercise();
+  const navigate = useNavigate();
+  const {token} = useTokenContext();
+  console.log(token)
 
   return (
-    <button className='logoutButton'
+    <button id='deleteButton'
       onClick={() => {
         console.log(id);
-        
-    deleteExerciseID(id);
+        deleteExerciseID(id,token);
+        navigate("/");
     }}
     >
       <img src={Trash } alt="Logout" height ="64" width="64" />
