7b850d8a02a19ac7907e5f638d9d4944ad330181
David <dbarcenahernandez@gmail.com>
2022-09-26 15:56:32 +0200

    logout button ajuste

--- src/components/LogoutButton/LogoutButton.js
+++ src/components/LogoutButton/LogoutButton.js
@@ -1,24 +1,25 @@
 import './LogoutButton.css'
-import { useTokenContext } from "../../contexts/TokenContext";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import IconLogout from "../../assets/imagenes/Logout_37127.png"
+import { useTokenContext } from '../../contexts/TokenContext'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import IconLogout from '../../assets/imagenes/Logout_37127.png'
 
 const LogoutButton = () => {
-  const { setToken } = useTokenContext();
-  const navigate = useNavigate();
+    const { setToken } = useTokenContext()
+    const navigate = useNavigate()
 
-  return (
-    <button className='logoutButton'
-      onClick={() => {
-        setToken("");
-        navigate("/");
-        toast.success("Unlogged succesfully :)");
-      }}
-    >
-      <img src={IconLogout } alt="Logout" height ="64" width="64" />
-    </button>
-  );
-};
+    return (
+        <button
+            className="logoutButton"
+            onClick={() => {
+                setToken('')
+                navigate('/')
+                toast.success('Unlogged succesfully :)')
+            }}
+        >
+            <img src={IconLogout} alt="Logout" height="60" width="60" />
+        </button>
+    )
+}
 
-export default LogoutButton;
+export default LogoutButton
