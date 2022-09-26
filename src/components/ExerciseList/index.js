71eac82ea670f65eebecbf1bccbc043dee024a95
davizdelarosa <108464629+davizdelarosa@users.noreply.github.com>
2022-09-26 16:27:07 +0200

    creacion de css para el navbar y fuente

--- src/components/ExerciseList/index.js
+++ src/components/ExerciseList/index.js
@@ -1,25 +1,25 @@
-import { Link } from "react-router-dom";
-import "./styles.css";
-import Exercise from "../Exercise";
-import useUser from "../../hooks/useUser";
+import { Link } from 'react-router-dom'
+import './styles.css'
+import Exercise from '../Exercise'
+import useUser from '../../hooks/useUser'
 /* import DeteleExerciseButton from "../DeleteExerciseButton/DeleteExerciseButton";
  */
 const ExercisesList = ({ exercises }) => {
-  const { user } = useUser();
-  return (
-    <ul className="exercise_list">
-      {exercises.map((exercise) => {
-        return (
-            /* <Link to={`?id=${exercise.id}`}> */
-            <li key={exercise.id}>
-               {/*user.type_user === 'admin' && <DeteleExerciseButton id={exercise.id}/>*/}
-              <Exercise exercise={exercise} />
-            </li>
-            /* </Link> */
-        );
-      })}
-    </ul>
-  );
-};
+    const { user } = useUser()
+    return (
+        <ul className="exercises_list">
+            {exercises.map((exercise) => {
+                return (
+                    /* <Link to={`?id=${exercise.id}`}> */
+                    <li key={exercise.id}>
+                        {/*user.type_user === 'admin' && <DeteleExerciseButton id={exercise.id}/>*/}
+                        <Exercise exercise={exercise} />
+                    </li>
+                    /* </Link> */
+                )
+            })}
+        </ul>
+    )
+}
 
-export default ExercisesList;
+export default ExercisesList
