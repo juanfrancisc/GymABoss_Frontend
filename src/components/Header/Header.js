33ce874282e085e054ac4948361973073be2f9d8
davizdelarosa <108464629+davizdelarosa@users.noreply.github.com>
2022-09-26 16:23:24 +0200

    modificacion de css en header y navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,11 @@
-import "./header.css";
-import { Link } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
-import { useTokenContext } from "../../contexts/TokenContext";
-
+import './header.css'
+import { Link } from 'react-router-dom'
+import Navbar from '../Navbar/Navbar'
+import { useTokenContext } from '../../contexts/TokenContext'
 
 const Header = () => {
-    const { token } = useTokenContext();
-  return (
-    <>
-        <Link to="/">
-            <h1>Gym a Boss</h1>
-        </Link>
+    const { token } = useTokenContext()
+    return <>{token && <Navbar />}</>
+}
 
-        {token && (
-            <Navbar />
-        )}
-    </>
-  );
-};
-
-export default Header;
+export default Header
