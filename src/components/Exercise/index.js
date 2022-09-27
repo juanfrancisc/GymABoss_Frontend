0347544b24b72929c08989809c8ab83fdf187847
davizdelarosa <108464629+davizdelarosa@users.noreply.github.com>
2022-09-27 09:16:57 +0200

    Modificados css links, titulos y color

--- src/components/Exercise/index.js
+++ src/components/Exercise/index.js
@@ -1,32 +1,30 @@
-import { Link } from "react-router-dom";
-import { useTokenContext } from "../../contexts/TokenContext";
-import { toast } from "react-toastify";
-import "./styles.css";
-import flexiones from "../../assets/imagenes/flexiones.jpg";
-import useUser from "../../hooks/useUser";
-import DeteleExerciseButton from "../DeleteExerciseButton/DeleteExerciseButton";
-import EditExerciseButton from "../ModifyExerciseButton/ModifyExerciseButton";
-import LikeButton from "../LikeButton/LikeButton";
-import Trash from "../../assets/imagenes/trash.png"
-import { useState } from "react";
-import EditExerciseForm from "../EditExerciseForm/EditExerciseForm";
-import Edit from "../../assets/imagenes/edit.png"
-import useExercises from "../../hooks/useExercises";
+import { Link } from 'react-router-dom'
+import { useTokenContext } from '../../contexts/TokenContext'
+import { toast } from 'react-toastify'
+import './styles.css'
+import flexiones from '../../assets/imagenes/flexiones.jpg'
+import useUser from '../../hooks/useUser'
+import DeteleExerciseButton from '../DeleteExerciseButton/DeleteExerciseButton'
+import EditExerciseButton from '../ModifyExerciseButton/ModifyExerciseButton'
+import LikeButton from '../LikeButton/LikeButton'
+import Trash from '../../assets/imagenes/trash.png'
+import { useState } from 'react'
+import EditExerciseForm from '../EditExerciseForm/EditExerciseForm'
+import Edit from '../../assets/imagenes/edit.png'
+import useExercises from '../../hooks/useExercises'
 
+const Exercise = ({ exercise, setExercise }) => {
+    /* console.log({exercise, setExercise}); */
+    const { id, idUser, n_like, title, description, photo, typology } = exercise
+    const { user } = useUser()
+    const { token } = useTokenContext()
+    const [loading, setLoading] = useState(true)
+    const [exercises, setExercises] = useState('')
 
-const Exercise = ({exercise, setExercise}) => {
-  /* console.log({exercise, setExercise}); */
-  const { id, idUser, n_like, title, description, photo, typology } = exercise;
-  const { user } = useUser();
-  const { token } = useTokenContext();
-  const [loading, setLoading] = useState(true);
-  const [exercises, setExercises] = useState("");
-
-  return (
- 
-    <section className="boxes">
-      {/* {user.type_user === 'admin' && <DeteleExerciseButton exercise={exercise}/> } */}
-      {/* {user.type_user === 'admin' && 
+    return (
+        <section className="boxes">
+            {/* {user.type_user === 'admin' && <DeteleExerciseButton exercise={exercise}/> } */}
+            {/* {user.type_user === 'admin' && 
       <button id='deleteButton'
         onClick={async () => {
           try {
@@ -55,13 +53,18 @@ const Exercise = ({exercise, setExercise}) => {
       </button>
       } */}
 
-      {user.type_user === 'admin' && <EditExerciseButton exercise={exercise} setExercise={setExercise}/> }
+            {user.type_user === 'admin' && (
+                <EditExerciseButton
+                    exercise={exercise}
+                    setExercise={setExercise}
+                />
+            )}
 
-      <Link to={`?id=${id}`}>
-      <h3 className="title">{title}</h3>
-      <p className="typology">{typology}</p>
-      <img height="180" src={flexiones}></img>
-      {/* {photo.length > 0 && (
+            <Link className="Link" to={`?id=${id}`}>
+                <h3 className="title">{title}</h3>
+                <p className="typology">{typology}</p>
+                <img height="180" src={flexiones}></img>
+                {/* {photo.length > 0 && (
         <img
           
           src={`${process.env.REACT_APP_API_URL}/imagenes/${photo}`}
@@ -69,47 +72,48 @@ const Exercise = ({exercise, setExercise}) => {
         />
       )} */}
 
-      <p className="nLikes">Likes: {n_like}</p>
-      </Link>
-      
-      <LikeButton id={id}/>
+                <p className="nLikes">Likes: {n_like}</p>
+            </Link>
 
-      <button type="button" onClick={ async () => {
-        
-        try {
-          const consulta = `${process.env.REACT_APP_API_URL}/addLike/${id}`
-          //console.log(consulta)
-  
-          const res = await fetch(consulta,{
-              method: "POST",
-              headers: {
-                authorization: token,
-              }
-          });
-          const body = await res.json();
-          //console.log(body)
-  
-          if (!res.ok) {
-            throw new Error(
-              "Unexpected error fetching API. Please, try again or contact support"
-            );
-          }
-  
-          toast.success(body.message)
-          setExercises({...exercises});
-          //console.log(like)
-        } catch (error) {
-          console.error(error.message);
-          toast.error(error.message);
-        } finally {
-          setLoading(false);
-        }
+            <LikeButton id={id} />
 
+            <button
+                type="button"
+                onClick={async () => {
+                    try {
+                        const consulta = `${process.env.REACT_APP_API_URL}/addLike/${id}`
+                        //console.log(consulta)
 
-      }}>Me gusta!</button>
-    </section>
+                        const res = await fetch(consulta, {
+                            method: 'POST',
+                            headers: {
+                                authorization: token,
+                            },
+                        })
+                        const body = await res.json()
+                        //console.log(body)
 
-  );
-};
+                        if (!res.ok) {
+                            throw new Error(
+                                'Unexpected error fetching API. Please, try again or contact support'
+                            )
+                        }
 
-export default Exercise;
+                        toast.success(body.message)
+                        setExercises({ ...exercises })
+                        //console.log(like)
+                    } catch (error) {
+                        console.error(error.message)
+                        toast.error(error.message)
+                    } finally {
+                        setLoading(false)
+                    }
+                }}
+            >
+                Me gusta!
+            </button>
+        </section>
+    )
+}
+
+export default Exercise
