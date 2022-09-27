6baac05dfa04ce81e093d78ee7a549589631a9c1
David <dbarcenahernandez@gmail.com>
2022-09-27 11:08:10 +0200

    HolaHola

--- src/components/ExerciseList/index.js
+++ src/components/ExerciseList/index.js
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom'
 import './styles.css'
-import { toast } from "react-toastify";
-import { useTokenContext } from "../../contexts/TokenContext";
-import Trash from "../../assets/imagenes/trash.png"
+import { toast } from 'react-toastify'
+import { useTokenContext } from '../../contexts/TokenContext'
+import Trash from '../../assets/imagenes/trash.png'
 import Exercise from '../Exercise'
 import useUser from '../../hooks/useUser'
 import { useState } from 'react'
@@ -10,9 +10,9 @@ import useExercises from '../../hooks/useExercises'
 /* import DeteleExerciseButton from "../DeleteExerciseButton/DeleteExerciseButton";
  */
 const ExercisesList = () => {
-    const {exercises, setExercises} = useExercises([]);
+    const { exercises, setExercises } = useExercises([])
     const { user } = useUser()
-    const { token } = useTokenContext();
+    const { token } = useTokenContext()
     const [updateExercises, setUpdateExercises] = useState([exercises])
 
     return (
@@ -21,40 +21,52 @@ const ExercisesList = () => {
                 return (
                     /* <Link to={`?id=${exercise.id}`}> */
                     <li key={exercise.id}>
-                        {user.type_user === 'admin' && 
-                            <button id='deleteButton' value={exercise.name}
-                            onClick={async () => {
-                                
-                            try {
-                                const res = await fetch(`${process.env.REACT_APP_API_URL}/deleteExercise/${exercise.id}`, {
-                                method: "DELETE",
-                                headers: {
-                                    authorization: token,
-                                }
-                                })
-                                //console.log(res);
+                        {user.type_user === 'admin' && (
+                            <button
+                                id="deleteButton"
+                                value={exercise.name}
+                                onClick={async () => {
+                                    try {
+                                        const res = await fetch(
+                                            `${process.env.REACT_APP_API_URL}/deleteExercise/${exercise.id}`,
+                                            {
+                                                method: 'DELETE',
+                                                headers: {
+                                                    authorization: token,
+                                                },
+                                            }
+                                        )
+                                        //console.log(res);
 
-                                const body = await res.json();
-                                //console.log(body)                                
-                                
-                                if (!res.ok) {
-                                    throw new Error(body.message);
-                                }
+                                        const body = await res.json()
+                                        //console.log(body)
 
-                                const updateExercises = exercises.filter((item) => item !== exercise.id)
-                                console.log(updateExercises)
-                                
-                                setExercises([...updateExercises])
-                                toast.success(body.message)
-                            } catch (error) {
-                                console.error(error.message);
-                                toast.error(error.message);
-                            }
-                            }}
+                                        if (!res.ok) {
+                                            throw new Error(body.message)
+                                        }
+
+                                        const updateExercises =
+                                            exercises.filter(
+                                                (item) => item !== exercise.id
+                                            )
+                                        console.log(updateExercises)
+
+                                        setExercises([...updateExercises])
+                                        toast.success(body.message)
+                                    } catch (error) {
+                                        console.error(error.message)
+                                        toast.error(error.message)
+                                    }
+                                }}
                             >
-                                <img src={Trash } alt="Logout" height ="64" width="64" />
+                                <img
+                                    src={Trash}
+                                    alt="Logout"
+                                    height="64"
+                                    width="64"
+                                />
                             </button>
-                        }
+                        )}
                         {/*user.type_user === 'admin' && <DeteleExerciseButton id={exercise.id}/>*/}
                         <Exercise exercise={exercise} />
                     </li>
