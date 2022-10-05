f185123de72fa398d8d73911dac1d44d64e5ae0b
Juanfrancisc <juanfrancisc@hotmail.com>
2022-10-05 15:28:52 +0200

    Añadida funcion de recuperar los ejercicios marcados como like

--- src/components/LikeButton/LikeButton.js
+++ src/components/LikeButton/LikeButton.js
@@ -2,17 +2,20 @@
 import './LikeButton.css';
 import { toast } from 'react-toastify';
 import { useTokenContext } from '../../contexts/TokenContext';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import heart from '../../assets/imagenes/black_heart.png'
+/* import { useNavigate } from 'react-router-dom'; */
+import { useState, useEffect } from 'react';
+/* import heart from '../../assets/imagenes/black_heart.png' */
 
 
 const LikeButton = ({ id, setExerciseLikes }) => {
     const { token } = useTokenContext();
-    const navigate = useNavigate();
-    const [like, setLike] = useState('');
+    /* const navigate = useNavigate();
+    const [like, setLike] = useState(''); */
+
+    const [verifyLike, setVerifyLike] = useState([]);
 
     const likeExerciseId = async (id, token) => {
+        setVerifyLike(verifyLike)
         try {
             const consulta = `${process.env.REACT_APP_API_URL}/addLike/${id}`;
 
@@ -28,21 +31,56 @@ const LikeButton = ({ id, setExerciseLikes }) => {
             toast.success(body.message);
 
             setExerciseLikes(id, body.likeCount);
+            
         } catch (error) {
             console.error(error.message);
             toast.error(error.message);
         }
     };
 
+    useEffect (() => {
+        const verifyLikeUser = async () => {
+            try {
+                const consulta = `${process.env.REACT_APP_API_URL}/like`
+            
+            const res = await fetch(consulta, {
+                method: 'GET',
+                headers: {
+                    authorization: token,
+                }
+            });
+    
+            const body = await res.json();
+    
+            /* console.log(body.data) */
+            setVerifyLike(body.data)
+            
+    
+            } catch (error) {
+                console.error(error.message);
+                toast.error(error.message);
+            }
+    
+        };
+        verifyLikeUser();
+    },[]);
+
     return (
         <>
-        <input type="image" 
-            /* src={heart} */
-            className="LikeButton"
+        {verifyLike.includes(id) ?  <input type="image" 
+            className="LikeButton_like"
             onClick={() => {
                 likeExerciseId(id, token);
+                setVerifyLike(verifyLike)
         }}
-        />
+        /> : <input type="image" 
+        className="LikeButton"
+        onClick={() => {
+            likeExerciseId(id, token);
+            setVerifyLike(verifyLike)
+        }}
+        />}
+        
             {/* <button
                 className="LikeButton"
                 id={id}
